502eef65034f04efe8e57fb41042465eefcd260f
IlyaNikk <vic096@yandex.ru>
2017-04-15 20:19:39 +0300

    Finish html and styles

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry:[
+		'./public/main.js'
+	],
+	output: {
+		path: './dist',
+		filename: path.join('assets', 'js', 'bundle.js'),
+		publicPath: '/'
+	},
+	module: {
+		loaders:[
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets:['env']
+				}
+			},
+			{
+				test: /\.test.css$/,
+				loader: 'css-loader'
+			},
+		]
+	},
+	resolve: {
+		alias: {}
+	},
+	plugins: [
+		new CleanWebpackPlugin('dist'),
+		new webpack.LoaderOptionsPlugin({
+			debug : true
+		}),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new HtmlPlugin({
+			filename: 'index.html',
+			template: path.resolve(__dirname, 'public/index.html')
+		})
+	]
+};
